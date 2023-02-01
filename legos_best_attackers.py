2a4c4b9820c5cd87f250526761db5922f0783976
Sasa Djukic <djukicmsasa@gmail.com>
2023-02-01 11:31:02 +0100

    add share of legendaries among best attackers

---
+++ legos_best_attackers.py
@@ -0,0 +1,39 @@
+
+
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+# sort data 
+poki = pd.read_csv('pokemon_data.csv', index_col='#')
+poki.rename(columns = {'Type 1' : 'Type_1', 'Type 2' : 'Type_2', 'Sp. Atk' : 'SP_Atk', 'Sp. Def' : 'SP_Def'}, inplace=True)
+poki.drop('Generation', 1, inplace=True)
+
+def legendary_among_best_attackers() -> None:
+
+    '''percentage of legendary poekmons among best attackers'''
+
+    attack = poki.sort_values(['Attack'], ascending=[0]).head(50)
+    legos = attack.loc[attack['Legendary'] == True].count()[-1]
+    non_legos = 50 - legos 
+
+    fig = plt.subplots()
+    plt.title(
+              'Percentage of legendary Pokemons among 50 best attackers', 
+              fontsize=18, 
+              fontweight='bold'
+             )
+
+    labels = ['Legendary', 'Non-legendary']
+    colors = ['#FB9300', '#674188']
+
+    plt.pie(
+            [legos, non_legos], 
+            labels=labels, 
+            colors=colors, 
+            autopct='%.2f %%'
+           )
+
+    plt.legend()
+    plt.show()
+    
+legendary_among_best_attackers()
